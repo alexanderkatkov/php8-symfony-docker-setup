65b9b2ea156cf20e7156f54a869c5cd27224e6f4
Alex Katkov <alexander.katkov@outlook.com>
2022-08-31 01:03:07 +0300

    removed nginx logs and solved file permission problems

--- docker/php/40-entrypoint.sh
+++ docker/php/40-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX ${APP_PATH}/var
+setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX ${APP_PATH}/var
 
 exec docker-php-entrypoint "$@"
