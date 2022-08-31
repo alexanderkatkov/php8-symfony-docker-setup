5b5cbba635404a645248fe375ab14352cafebcf1
Alex K <alexander.katkov@outlook.com>
2022-08-31 18:49:15 +0300

    moved supervisor to entrypoint, removed var file permissions obsolete fix

--- docker/php/40-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX ${APP_PATH}/var
-setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX ${APP_PATH}/var
-
-exec docker-php-entrypoint "$@"
---
+++ docker/php/zz-docker-entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+
+exec docker-php-entrypoint "$@"
