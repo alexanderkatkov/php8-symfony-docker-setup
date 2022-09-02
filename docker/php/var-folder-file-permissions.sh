1405622be661687ccf4a2e5cc1e47bd33eae23e4
Alex K <alexander.katkov@outlook.com>
2022-09-02 23:58:18 +0300

    proper VAR folder file permissions handling

---
+++ docker/php/var-folder-file-permissions.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+if [ -d "${APP_PATH}/var" ]; then
+  setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX "${APP_PATH}/var"
+  setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX "${APP_PATH}/var"
+fi
--- docker/php/zz-docker-entrypoint.sh
+++ docker/php/zz-docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+/bin/var/var-folder-file-permissions.sh
+
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 
 exec docker-php-entrypoint "$@"
