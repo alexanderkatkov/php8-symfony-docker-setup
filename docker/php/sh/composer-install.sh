cbdf0f208499685234109378f8410bc04df5fa3f
Alex K <alexander.katkov@outlook.com>
2022-09-22 01:21:56 +0300

    composer install on php container start

---
+++ docker/php/sh/composer-install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+
+if [[ -f "${APP_PATH}/composer.json" ]]; then
+  composer install ${COMPOSER_INSTALL_MODE}--classmap-authoritative
+fi
--- docker/php/sh/zz-docker-entrypoint.sh
+++ docker/php/sh/zz-docker-entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 /usr/local/bin/var-folder-file-permissions.sh
+# Run composer install as www-data
+su www-data -s /usr/local/bin/composer-install.sh
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 
